a880a61b6fbe7a1ca57cae5e0244ca6fa1f102d6
hodu31 <sungsam9709@gmail.com>
2022-06-03 16:03:37 +0900

    d

--- function/app.js
+++ function/app.js
@@ -169,3 +169,13 @@ const hen = {
         return "EGG";
     }
 }
+
+function yell(msg) {
+    try {
+        console.log(msg.toUpperCase().repeat(3));
+    } catch (e) {
+        console.log(e);
+        console.log("Please pass a string next time!")
+    }
+}
+
